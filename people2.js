7a0378919ced5f38cc7ff22f347d21ea112d2be9
Niki Fereidooni <nikifereidooni@gmail.com>
2022-05-02 19:41:10 -0400

    Revert "Update people2.js"
    This reverts commit 5b76f2f32e844f94afadd08dc68854d672105675.

--- people2.js
+++ people2.js
@@ -1,8 +1,8 @@
 const PEOPLE = [
-  {firstName: 'Chris', animal: 'dog', show: 'the wire'},
-  {firstName: 'Darga', animal: 'owl', show: 'fuckboy island'},
-  {firstName: 'Loren', animal: 'cat', show: 'steven universe'},          
-  {firstName: 'Niki', animal: 'sloth', show: 'our flag means death'},
-  {firstName: 'Pasha', animal: 'lion', show: 'bojack horseman'},
-  {firstName: 'Wolf', animal: 'wolf', show: 'the good place'},
+  {firstName: 'Chris', animal: 'dog'},
+  {firstName: 'Darga', animal: 'owl'},
+  {firstName: 'Loren', animal: 'cat'},           
+  {firstName: 'Niki', animal: 'sloth'},
+  {firstName: 'Pasha', animal: 'lion'},  
+  {firstName: 'Wolf', animal: 'wolf'},
 ];
