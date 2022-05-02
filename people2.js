49432cab0f6937e2984df67981b554b4357212d0
Niki Fereidooni <nikifereidooni@gmail.com>
2022-05-02 19:44:59 -0400

    added foods

--- people2.js
+++ people2.js
@@ -1,8 +1,8 @@
 const PEOPLE = [
-  {firstName: 'Chris', animal: 'dog'},
-  {firstName: 'Darga', animal: 'owl'},
-  {firstName: 'Loren', animal: 'cat'},           
-  {firstName: 'Niki', animal: 'sloth'},
-  {firstName: 'Pasha', animal: 'lion'},  
-  {firstName: 'Wolf', animal: 'wolf'},
+  {firstName: 'Chris', animal: 'dog', food: 'bacon'},
+  {firstName: 'Darga', animal: 'owl', food: 'sandwich'},
+  {firstName: 'Loren', animal: 'cat', food: 'sushi'},           
+  {firstName: 'Niki', animal: 'sloth', food: 'olives'},
+  {firstName: 'Pasha', animal: 'lion', food: 'burger'},  
+  {firstName: 'Wolf', animal: 'wolf', food: 'french fries'},
 ];
