09f5c74221014d871dd1e133e174d3d93cfb2a05
Niki Fereidooni <nikifereidooni@gmail.com>
2022-05-02 20:00:11 -0400

    made a bunch of changes

--- people2.js
+++ people2.js
@@ -1,8 +1,10 @@
 const PEOPLE = [
   {firstName: 'Chris', animal: 'dog', show: 'the wire'},
+  ['apples', 'pears', 'oranges'],
   {firstName: 'Darga', animal: 'owl', show: 'fuckboy island'},
-  {firstName: 'Loren', animal: 'cat', show: 'steven universe'},          
-  {firstName: 'Niki', animal: 'sloth', show: 'our flag means death'},
-  {firstName: 'Pasha', animal: 'lion', show: 'bojack horseman'},
-  {firstName: 'Wolf', animal: 'wolf', show: 'the good place'},
+  {firstName: 'Loren', animal: 'cat', occupation: 'developer', show: 'steven universe'},          
+  {firstName: 'Niki', animal: 'sloth', occupation: "developer", show: 'our flag means death'},
+  {lastName: 'Cornelius', birthyear: '1965', occupation: 'accountant'},
+  {firstName: 'Pash', animal: 'lion', show: 'bojack horseman'},
+  {firstName: 'Wolfie', animal: 'wolf', show: 'the good place'},
 ];
