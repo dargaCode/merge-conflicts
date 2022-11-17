406e68ea558870afff6e20fe3ab5c404d48276b9
Darga Darga <github@dargacode.com>
2022-11-16 20:38:14 -0700

    delete frog

--- people2.js
+++ people2.js
@@ -2,7 +2,6 @@ const PEOPLE = [
   { animal: 'dog', firstName: 'Chris', middleName: 'Mercutio', favoriteFood: 'bacon', show: 'The Wire'},
   { animal: 'cat', firstName: 'Loren', occupation: 'developer', favoriteFood: 'sushi', show: 'Steven Universe'},           
   { animal: 'sloth', firstName: 'Niki', occupation: 'developer', middleName: 'Esmerelda', favoriteFood: 'olives', show: 'Our Flag Means Death'},
-  { animal: 'frog', firstName: 'Cornelius', birthYear: 1965, occupation: 'accountant'},
   { animal: 'lion', firstName: 'Pash', favoriteFood: 'burger', show: 'Bojack Horseman'},
   { animal: 'parrot', firstName: 'Peri', favoriteFood: 'couscous', show: 'The Walking Dead'}, 
   { animal: 'wolf', firstName: 'Wolfie', middleName: 'Leopold', favoriteFood: 'french fries', show: 'The Good Place'},
