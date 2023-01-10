bdbd7f8d0d5a9c1de28c7dc59b38823bb69e866f
Kailey <k.kotynski@gmail.com>
2023-01-09 17:56:35 -0800

    Reformatted list
    List reformatted for style

--- people2.js
+++ people2.js
@@ -1,12 +1,58 @@
 const PEOPLE = [
-  { animal: 'dog', firstName: 'Chris', middleName: 'Mercutio', favoriteFood: 'bacon', show: 'The Wire'},
-  { animal: 'cat', firstName: 'Loren', occupation: 'developer', favoriteFood: 'sushi', show: 'Steven Universe'},           
-  { animal: 'sloth', firstName: 'Niki', occupation: 'developer', middleName: 'Esmerelda', favoriteFood: 'olives', show: 'Our Flag Means Death'},
-  { animal: 'lion', firstName: 'Pash', favoriteFood: 'burger', show: 'Bojack Horseman'},
-  { animal: 'parrot', firstName: 'Peri', favoriteFood: 'couscous', show: 'The Walking Dead'}, 
-  { animal: 'wolf', firstName: 'Wolfie', middleName: 'Leopold', favoriteFood: 'french fries', show: 'The Good Place'},
-  { animal: 'deer', firstName: 'Lily', middleName: 'Jane', show: 'Amazing Race', favoriteFood: 'pizza'},
-  { animal: 'bunny', firstName: 'Buns', middleName: 'Bunner', show: 'Survivor'}
+  {
+    animal: "dog",
+    firstName: "Chris",
+    middleName: "Mercutio",
+    favoriteFood: "bacon",
+    show: "The Wire",
+  },
+  {
+    animal: "cat",
+    firstName: "Loren",
+    occupation: "developer",
+    favoriteFood: "sushi",
+    show: "Steven Universe",
+  },
+  {
+    animal: "sloth",
+    firstName: "Niki",
+    occupation: "developer",
+    middleName: "Esmerelda",
+    favoriteFood: "olives",
+    show: "Our Flag Means Death",
+  },
+  {
+    animal: "lion",
+    firstName: "Pash",
+    favoriteFood: "burger",
+    show: "Bojack Horseman",
+  },
+  {
+    animal: "parrot",
+    firstName: "Peri",
+    favoriteFood: "couscous",
+    show: "The Walking Dead",
+  },
+  {
+    animal: "wolf",
+    firstName: "Wolfie",
+    middleName: "Leopold",
+    favoriteFood: "french fries",
+    show: "The Good Place",
+  },
+  {
+    animal: "deer",
+    firstName: "Lily",
+    middleName: "Jane",
+    show: "Amazing Race",
+    favoriteFood: "pizza",
+  },
+  {
+    animal: "bunny",
+    firstName: "Buns",
+    middleName: "Bunner",
+    show: "Survivor",
+  },
   {
     animal: "horse",
     firstName: "Royal",
@@ -14,4 +60,3 @@ const PEOPLE = [
     show: "Slow Horses",
   },
 ];
-
