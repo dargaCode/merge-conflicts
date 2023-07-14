71079a251ad252ed8682a0f73a8d5d9329650ed0
Darga Darga <github@dargacode.com>
2023-07-13 19:19:59 -0600

    Revert "oh no i deleted all of the files"
    This reverts commit ef92df7090db0597242cf9ca46ece0d0ed919229.

---
+++ people2.js
@@ -0,0 +1,50 @@
+const PEOPLE = [
+  {
+    animal: "dog",
+    firstName: "Chris",
+    favoriteFood: "bacon",
+    show: "The Wire",
+  },
+  {
+    animal: "cat",
+    firstName: "Loren",
+    occupation: "developer",
+    favoriteFood: "sushi",
+    show: "Steven Universe",
+  },
+  {
+    animal: "sloth",
+    firstName: "Niki",
+    occupation: "developer",
+    favoriteFood: "carrots",
+    show: "Our Flag Means Death",
+  },
+  {
+    animal: "lion",
+    firstName: "Pash",
+    favoriteFood: "burger",
+    show: "Bojack Horseman",
+  },
+  {
+    animal: "parrot",
+    firstName: "Peri",
+    favoriteFood: "couscous",
+    show: "Jeopardy",
+  },
+  {
+    animal: "wolf",
+    firstName: "Wolfie",
+    favoriteFood: "french fries",
+    show: "The Good Place",
+  },
+  {
+    animal: "bunny",
+    firstName: "Buns",
+    show: "Survivor",
+  },
+  {
+    animal: "horse",
+    firstName: "Royal",
+    show: "Slow Horses",
+  },
+];
